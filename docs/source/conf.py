40a521c87b376a7261ddcc2decadb04401a0951d
shiva-ram-godar <shiva@fusemachines.com>
2025-07-08 15:17:51 +0545

    src issue fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,9 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
