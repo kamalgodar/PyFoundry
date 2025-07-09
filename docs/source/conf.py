b0345de7c5d83babc6fed4b42e6edb2a7ebca2f3
shiva-ram-godar <shiva@fusemachines.com>
2025-07-09 14:39:30 +0545

    resolve the issue of init not being able to render the source module

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,12 +8,27 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
+import toml
 
-project = 'pyfoundry'
-copyright = '2025, fusemachines'
-author = 'fusemachines'
-release = '0.1'
+sys.path.insert(0, os.path.abspath("../"))
+
+def _get_project_meta():
+    try:
+        pyproject_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "../..", "pyproject.toml")
+        )
+        return toml.load(pyproject_path)["project"]
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Could not find pyproject.toml at {pyproject_path}")
+
+
+pkg_meta = _get_project_meta()
+project = pkg_meta["name"]
+copyright = "2025, Kamal Godar"
+author = pkg_meta["authors"][0]["name"]
+
+version = pkg_meta["version"]
+release = version
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
